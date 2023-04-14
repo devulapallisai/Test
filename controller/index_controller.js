f461d1633fccc89341ca2b28bc1798409fc59776
devulapallisai <ee20btech11013@iith.ac.in>
2023-04-14 14:00:44 +0530

    added

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -1,17 +1,17 @@
-const { v4: uuidv4 } = require('uuid'); //used to get unique room ids
+const { v4: uuidv4 } = require("uuid"); //used to get unique room ids
 
 module.exports.renderHome = function (req, res) {
-    return res.render('home');
-}
+  return res.render("home");
+};
 
 module.exports.createNewRoom = function (req, res) {
-    return res.redirect(`/room/${uuidv4()}`);
-}
+  return res.redirect(`/room/${uuidv4()}`);
+};
 
 module.exports.joinRoom = function (req, res) {
-    return res.render('room', { roomId: req.params.room });
-}
+  return res.render("room", { roomId: req.params.room });
+};
 
 module.exports.leaveRoom = function (req, res) {
-    return res.render('meeting_end', { roomId: req.params.room });
-}
+  return res.render("meeting_end", { roomId: req.params.room });
+};
